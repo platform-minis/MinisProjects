00a33e8c15b01f9f6946e0d344d324e4cfe5aaaa
Marcin Hersztowski <mhersztowski@gmail.com>
2026-03-15 22:56:14 +0100

    wipo

--- src/Esp32S3Test/src/Test1/Test1.ino
+++ src/Esp32S3Test/src/Test1/Test1.ino
@@ -1,6 +1,6 @@
 void setup() {
   Serial.begin(115200);
-  while (!Serial) delay(10);  // wait for USB CDC to connect
+  while (!Serial && millis() < 3000);  // wait up to 3s for USB CDC
   Serial.println("ESP32-S3 Test - Hello!");
   Serial.printf("CPU freq: %d MHz\n", getCpuFrequencyMhz());
   Serial.printf("Flash size: %d MB\n", ESP.getFlashChipSize() / (1024 * 1024));
