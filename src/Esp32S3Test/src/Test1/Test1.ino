1404ac260cd6a97b28fb98fa3bdd6215341d50e2
Marcin Hersztowski <mhersztowski@gmail.com>
2026-03-15 22:52:13 +0100

    wip

--- src/Esp32S3Test/src/Test1/Test1.ino
+++ src/Esp32S3Test/src/Test1/Test1.ino
@@ -1,10 +1,6 @@
 void setup() {
   Serial.begin(115200);
-
-  // HWCDC (native USB) needs time to enumerate on the host
-  // Without this delay, early Serial.print() is lost
-  delay(2000);
-
+  while (!Serial) delay(10);  // wait for USB CDC to connect
   Serial.println("ESP32-S3 Test - Hello!");
   Serial.printf("CPU freq: %d MHz\n", getCpuFrequencyMhz());
   Serial.printf("Flash size: %d MB\n", ESP.getFlashChipSize() / (1024 * 1024));
